f1bd631b77b642102a2cdf05011c967c5eeef87f
Nooonamee <ducdonhu001@gmail.com>
2022-11-07 21:37:18 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+var red = $(".red")
+var yellow = $(".yellow")
+var green = $(".green")
+var sum = $(".sum")
+
+
+setInterval(()=> {
+    fetch("https://api.thingspeak.com/channels/1909224/feeds.json?results=5")
+    .then(res => res.json())
+    .then(res => {
+        console.log(res.feeds[0].field1)
+        a = parseInt(res.feeds[0].field1)
+        b = parseInt(res.feeds[0].field2)
+        c = parseInt(res.feeds[0].field3)
+        red.innerHTML = a
+        green.innerHTML = b
+        yellow.innerHTML = c
+        sum.innerHTML = a+b+c
+    })
+},5000)
