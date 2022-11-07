feb82af1816d9505ffdf8b22d52b8d088ecec9f5
Nooonamee <ducdonhu001@gmail.com>
2022-11-07 21:44:15 +0700

    fix delay

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ var yellow = $(".yellow")
 var green = $(".green")
 var sum = $(".sum")
 
+fetch("https://api.thingspeak.com/channels/1909224/feeds.json?results=5")
+    .then(res => res.json())
+    .then(res => {
+        console.log(res.feeds[0].field1)
+        a = parseInt(res.feeds[0].field1)
+        b = parseInt(res.feeds[0].field2)
+        c = parseInt(res.feeds[0].field3)
+        red.innerHTML = a
+        green.innerHTML = b
+        yellow.innerHTML = c
+        sum.innerHTML = a+b+c
+    })
 
 setInterval(()=> {
     fetch("https://api.thingspeak.com/channels/1909224/feeds.json?results=5")
